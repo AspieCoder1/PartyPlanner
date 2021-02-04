b9a768d0680f1fbaf3f9f61e1e31a5448a93d3ab
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-02-04 22:36:47 +0000

    Documentation added

---
+++ docs/doczrc.js
@@ -0,0 +1,8 @@
+export default {
+    title: 'X1 Backend Services',
+    themeConfig: {
+        initialColorMode: 'dark',
+    },
+    src: './docs/',
+    menu: ['Getting Started', {name: 'API Reference', menu: ['Users']}]
+}
