c6be57562f14bcf741116a40a458c1602954fbd6
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-02-10 17:19:48 +0000

    User model test suite finished

--- api/tests/models/user.test.ts
+++ api/tests/models/user.test.ts
@@ -1,5 +1,6 @@
 import { User, IUser } from '../../src/models/user';
 import '../../src/db/mongoose';
+import { ObjectId } from 'mongodb';
 
 afterEach(async () => {
   await User.remove({});
@@ -8,6 +9,7 @@ afterEach(async () => {
 describe('User model tests', () => {
   it('Should correctly add a user to the db', async () => {
     const newUser = {
+      _id: new ObjectId(),
       email: 'test@example.com',
       password: '24355365644faf',
       username: 'test',
@@ -16,6 +18,8 @@ describe('User model tests', () => {
     const user: IUser = new User(newUser);
     const savedUser = await user.save();
     expect(user).toEqual(savedUser);
+    const foundUser = await User.findById(newUser._id);
+    expect(foundUser).toBeTruthy();
   });
 
   it('Should throw error if email not provided', async () => {
@@ -132,6 +136,7 @@ describe('User model tests', () => {
 
   it('Should save user for DB if dietary requirements set', async () => {
     const newUser = {
+      _id: new ObjectId(),
       email: 'test@example.com',
       password: '24355365644faf',
       username: 'test',
@@ -141,10 +146,15 @@ describe('User model tests', () => {
     const user: IUser = new User(newUser);
     const savedUser = await user.save();
     expect(user).toEqual(savedUser);
+
+    const foundUser = await User.findById(newUser._id);
+    expect(foundUser).toBeTruthy();
+    expect(foundUser.dietary_requirements).toBe(newUser.dietary_requirements);
   });
 
   it('Should save user for DB if accessibility requirements set', async () => {
     const newUser = {
+      _id: new ObjectId(),
       email: 'test@example.com',
       password: '24355365644faf',
       username: 'test',
@@ -154,6 +164,12 @@ describe('User model tests', () => {
     const user: IUser = new User(newUser);
     const savedUser = await user.save();
     expect(user).toEqual(savedUser);
+
+    const foundUser = await User.findById(newUser._id);
+    expect(foundUser).toBeTruthy();
+    expect(foundUser.accessibility_requirements).toBe(
+      newUser.accessibility_requirements
+    );
   });
 
   it('Should raise error if email is not unique', async () => {
@@ -230,5 +246,24 @@ describe('User model tests', () => {
     }
     expect(error).not.toBeNull();
     expect(error.errors.phone_num.message).toBe('phone number is invalid');
+  });
+
+  it('Should save user for DB if phone number requirements set', async () => {
+    const newUser = {
+      _id: new ObjectId(),
+      email: 'test@example.com',
+      password: '24355365644faf',
+      username: 'test',
+      accessibility_requirements: 'wheelchair user',
+      phone_num: '+447821337080',
+    };
+
+    const user: IUser = new User(newUser);
+    const savedUser = await user.save();
+    expect(user).toEqual(savedUser);
+
+    const foundUser = await User.findById(newUser._id);
+    expect(foundUser).toBeTruthy();
+    expect(foundUser.phone_num).toBe(newUser.phone_num);
   });
 });
