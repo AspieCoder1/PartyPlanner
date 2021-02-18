1f85b0b7418defc9bdf6d99612c0d72d4420c2b0
t11915jr <jeremy.roe@student.manchester.ac.uk>
2021-02-18 11:19:51 +0000

    Created the about.html page

---
+++ api/src/models/party.ts
@@ -0,0 +1,55 @@
+import { Schema, Document, ObjectId, Model, model } from 'mongoose';
+import validator from 'validator';
+
+export interface IParty extends Document {
+  organiser: string;
+  description: string;
+  location: string;
+  date: string;
+  ageRate: string;
+  attendeesID?: string[];
+  todoID?: string;
+}
+
+const userSchema: Schema = new Schema<any>({
+  organiser: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    minlength: 7,
+    required: true,
+  },
+  location: {
+    type: String,
+    trim: true,
+    required: [true, 'A location is required']
+  },
+  date: {
+    type: Date,
+    required: [true, 'A Date is required'],
+    validate: {
+        validator: validator.isDate,
+        message: 'Invalid Date',
+    }
+  },
+  ageRate: {
+    type: Boolean,
+    required: [true, 'An age rating is required']
+  },
+  attendeesID: {
+    type: [Schema.Types.ObjectId],
+    required: false,
+    ref: 'User',
+    default: [],
+  },
+  todoID: {
+    type: String,
+    required: false,
+    ref: 'Todo'
+  }
+});
+
+export const Party: Model<IParty> = model('party', userSchema);
