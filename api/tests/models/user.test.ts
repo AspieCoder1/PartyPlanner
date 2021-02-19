49eb51a40cd165aa7fe2b3a3170c78b1d568d23e
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-02-19 16:16:17 +0000

    Reformatted user model test file

--- api/tests/models/user.test.ts
+++ api/tests/models/user.test.ts
@@ -9,7 +9,7 @@ beforeAll(async () => {
   mongoServer = new MongoMemoryServer();
   const mongoUri = await mongoServer.getUri();
   await mongoose
-    .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true  })
+    .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then()
     .catch(() => console.error('Unable to connect to MongoDB'));
 });
