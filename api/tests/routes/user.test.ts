a44e7f3b4496a7308ca24779315db1a4fc8104e2
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-02-24 18:13:30 +0000

    Added JWT authorisation for passport

--- api/tests/routes/user.test.ts
+++ api/tests/routes/user.test.ts
@@ -1,9 +1,27 @@
 import userRouter from '../../src/routes/user';
 import * as express from 'express';
 import * as request from 'supertest';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import * as mongoose from "mongoose";
 
 const app: express.Application = express();
 app.use('/', userRouter);
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getUri();
+  await mongoose
+    .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then()
+    .catch(() => console.error('Unable to connect to MongoDB'));
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
 
 describe('GET /', () => {
   it('Responds with 200', async () => {
