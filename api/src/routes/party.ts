e297b29bd8535d6e14969ded571be7b251dee0c0
divinjacob3 <divinjacob3@gmail.com>
2021-03-03 19:52:16 +0000

    Made the update party route, not entriley sure if it's correct, please can someone look over it

--- api/src/routes/party.ts
+++ api/src/routes/party.ts
@@ -63,7 +63,10 @@ partyRouter.post('/create',
       res.status(500).json('Oops something went wrong');
     }
 
-  });
+  }
+);
+
+
 
 partyRouter.delete('/:id',
   async (req: express.Request, res: express.Response) => {
@@ -81,7 +84,72 @@ partyRouter.delete('/:id',
     catch (e) {
       res.status(500).json('Oops something went wrong');
     }
-    
-  });
+  }
+);
 
-  export default partyRouter;
+partyRouter.post('/:id/update',
+  async (req: express.Request, res: express.Response) => {
+
+    const name = req.body.name ? req.body.name : '';
+    const organiser = req.body.organiser ? req.body.organiser : '';
+    const description = req.body.description ? req.body.description : '';
+    const location = req.body.location ? req.body.location : '';
+    const date = Date.parse(req.body.date) ? req.body.date : '';
+    const time = req.body.time ? req.body.time : '';
+    const ageRate = req.body.ageRate ? req.body.ageRate : '';
+    const attendeesID = req.body.attendeesID ? req.body.attendeesID : [];  
+    const todoID = req.body.todoID ? req.body.todoID : '';
+      
+    const party = {
+      name,
+      organiser,
+      description,
+      location,
+      date,
+      time,
+      ageRate,
+      attendeesID,
+      todoID
+    }
+
+    const errors = validateNewParty(party);
+    if (!_.isEmpty(errors)) {
+      return res.status(400).json(errors);
+    }
+
+    try {
+      const updatedParty: IParty = new Party({
+        name: party.name,
+        organiser: party.organiser,
+        description: party.description,
+        location: party.location,
+        date: party.date,
+        time: party.time,
+        ageRate: party.ageRate,
+        attendeesID: party.attendeesID,
+        todoID: party.todoID
+      });
+      const updatingPartyID = req.params.id;
+      const foundParty = await Party.findById(updatingPartyID);
+      if (foundParty) {
+        const savedParty = await Party.findOneAndUpdate({ _id: updatingPartyID }, updatedParty);
+				res.status(200).json(savedParty);
+      } else { 
+        res
+          .status(400)
+          .send('An exact party like this already exists');}
+    }
+    catch (e) {
+      res.status(500).json('Oops something went wrong');
+    }
+
+  }
+);
+
+
+
+
+
+
+
+export default partyRouter;
