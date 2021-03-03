45d32b82113207ef72f87b26e81d6716cb2ffaef
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-03-03 22:30:11 +0000

    Wired up modal and added validation

---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import { useFormik } from "formik";
+import styles from "./LoginForm.module.scss";
+import * as Yup from "yup";
+import CSS from "csstype";
+
+const buttonStyles: CSS.Properties = {
+  color: "#ddd9da",
+  border: "none",
+  background: "none",
+};
+
+const submitButton: CSS.Properties = {
+  background: "#6f3473",
+  border: "none",
+  color: "white",
+  fontSize: "24px",
+  marginTop: "16px",
+  padding: "8px",
+};
+
+type IProps = {
+  closeModal: () => void;
+};
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
+export const LoginForm = (props: IProps) => {
+  const initialValues: LoginFormValues = {
+    email: "",
+    password: "",
+  };
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid Email").required("Required"),
+    password: Yup.string()
+      .min(7, "Password must have at least 7 characters")
+      .max(20, "Password can have at most 20 characters")
+      .required("Required"),
+  });
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema: LoginSchema,
+  });
+
+  return (
+    <div>
+      <div className={styles.header}>
+        <h1>Login Form</h1>
+        <button
+          style={buttonStyles}
+          className={styles.closeModal}
+          onClick={props.closeModal}
+        >
+          X
+        </button>
+      </div>
+
+      <form className={styles.form} onSubmit={formik.handleSubmit}>
+        <input
+          className={styles.input}
+          type="text"
+          name="email"
+          placeholder="e-mail"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        {formik.errors.email && formik.touched.email ? (
+          <p id="emailError" className={styles.error}>{formik.errors.email}</p>
+        ) : null}
+        <input
+          className={styles.input}
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        {formik.errors.password && formik.touched.password ? (
+          <p id="passwordError" className={styles.error}>{formik.errors.password}</p>
+        ) : null}
+        <button style={submitButton} type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
