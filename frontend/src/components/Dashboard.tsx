5b5eb7d522c6396db475d08b117dbe82aad998b1
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-03-19 09:54:47 +0000

    Connected Dashboard component

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Store } from '../redux/store';
+import { UserState } from '../redux/user-slice';
 
-export default class Dashboard extends React.Component {
+type IProps = {
+	user: UserState;
+};
+
+export class Dashboard extends React.Component<IProps, never> {
 	render(): React.ReactNode {
 		return (
 			<div>
 				<h1>Dashboard component</h1>
+				<p>Hello {this.props.user.userName}</p>
 			</div>
 		);
 	}
 }
+
+const mapStateToProps = (state: Store) => ({ user: state.user });
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
