315c8fb3bce4ba34a8f7c2568f2d89d3ac6c30f5
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-03-19 12:29:39 +0000

    Added username rendering and tests for dashboard component

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Store } from '../redux/store';
 import { UserState } from '../redux/user-slice';
+import styles from './Dashboard.module.scss';
+import Header from './Header';
 
 type IProps = {
 	user: UserState;
@@ -10,10 +12,14 @@ type IProps = {
 export class Dashboard extends React.Component<IProps, never> {
 	render(): React.ReactNode {
 		return (
-			<div>
-				<h1>Dashboard component</h1>
-				<p>Hello {this.props.user.userName}</p>
-			</div>
+			<React.Fragment>
+				<Header />
+				<div className={styles.container}>
+					<h1 className={styles.title}>Hello, {this.props.user.userName}</h1>
+					<p className={styles.paragraph}>The rest of the user dashboard content goes here.</p>
+				</div>
+
+			</React.Fragment>
 		);
 	}
 }
