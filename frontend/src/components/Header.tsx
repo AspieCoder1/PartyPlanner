a82cf2adf6646abe8b966f6d162bc5f10a9ce73a
t11915jr <jeremy.roe@student.manchester.ac.uk>
2021-03-24 14:26:36 +0000

    Added Logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import styles from './Header.module.scss';
+import logo from '../img/partyplanner.jpg';
 
 const Header = () => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.container}>
-				<h1 className={styles.title}>PartyPlanner.io</h1>
+				{/*console.log(logo);*/}
+				<h1 className={styles.title}><img src={logo} alt="Logo" /></h1>
 				<div className={styles.headerButtons}>
 					<button className={styles.logoutButton}>log out</button>
 				</div>
