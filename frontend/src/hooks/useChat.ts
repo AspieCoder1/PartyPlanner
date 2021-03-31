9e83bf404f94901ff65bf8627b0882e78d4af756
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-03-31 12:02:29 +0100

    Added a create message function

--- frontend/src/hooks/useChat.ts
+++ frontend/src/hooks/useChat.ts
@@ -1,8 +1,13 @@
 import { useEffect, useState, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const useChat = (chatID: string) => {
-	const [messages, setMessages] = useState([]);
+type HookReturn = {
+	messages: any[];
+	sendMessage: (msgBody: string) => void;
+};
+
+const useChat = (chatID: string): HookReturn => {
+	const [messages, setMessages] = useState<string[]>([]);
 	const socketRef = useRef<Socket>();
 
 	useEffect(() => {
@@ -11,14 +16,26 @@ const useChat = (chatID: string) => {
 			console.log('hello from server');
 		});
 
+		socketRef.current?.on('new_msg', (msg: string) => {
+			setMessages(messages => ([...messages, msg]));
+		});
+
 		// This now disconnects the socket and cleans everything up
 		// Prevents us from having a hanging connection
 		return () => {
+			console.log('disconnecting');
 			socketRef.current?.disconnect();
 		};
 	}, [chatID]);
 
-	return { messages };
+	const sendMessage = (msgBody: string): void => {
+		socketRef.current?.emit('new_msg', {
+			msg: msgBody,
+			room: chatID,
+		});
+	};
+
+	return { messages, sendMessage };
 };
 
 export default useChat;
