a885592a0751cb9199383d9f467d54acad86d446
Sayed Sayed <sayed.sayed@student.manchester.ac.uk>
2021-04-06 16:38:12 +0100

    Image model and routes

---
+++ api/src/models/image.ts
@@ -0,0 +1,29 @@
+import { Schema, Document, Model, model } from 'mongoose';
+
+export interface IImage extends Document {
+    userId: string;
+	partyId: string;
+    link: string;
+
+}
+
+const imageSchema: Schema = new Schema({
+	userId: {
+		type: String,
+		trim: true,
+        required : true,
+	},
+	partyId: {
+		type: String,
+		trim: true,
+		required : true,
+	},
+    link: {
+		type: String,
+		trim: true,
+		required : true,
+	},
+
+});
+
+export const Image: Model<IImage> = model('image', imageSchema);
---
+++ api/src/routes/image.ts
@@ -0,0 +1,45 @@
+import * as express from 'express';
+import * as _ from 'lodash';
+import { IImage, Image } from '../models/image';
+
+const imageRouter: express.Router = express.Router();
+
+export default imageRouter;
+
+
+//Upload Image 
+imageRouter.post(
+	'/upload/:uid/:pid/:link',
+
+	async (req: express.Request, res: express.Response) => {
+
+		try {
+			const newImage: IImage = new Image({
+				userId: req.params.uid,
+				partyId: req.params.pid,
+                link: req.params.link,
+			});
+		} catch (e) {
+			res.status(500).json('Oops something went wrong');
+		}
+	}
+);
+
+
+//Delete Image
+imageRouter.delete(
+	'/:id',
+	async (req: express.Request, res: express.Response) => {
+		const imageID = req.params.id;
+		try {
+			const delImage = await Image.findByIdAndDelete(imageID);
+			if (!delImage) {
+				res.status(404).send('No image of this id exists');
+			} else {
+				res.status(200).json(delImage);
+			}
+		} catch (e) {
+			res.status(500).json('Oops something went wrong');
+		}
+	}
+);
