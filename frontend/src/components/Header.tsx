5e6b278c30a0b02e44cf27d1300553978658cd74
AspieCoder <lukebraithwaite7@gmail.com>
2021-04-13 15:54:35 +0100

    Update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,18 +13,6 @@ type Props = {
 };
 
 const Header = (props: Props): JSX.Element => {
-	// state
-	const [searchModalOpen, setSearchModalOpen] = useState<boolean>(false);
-
-	// modal controls
-	const closeSearchModal = () => {
-		setSearchModalOpen(false);
-	};
-
-	const openSearchModal = () => {
-		setSearchModalOpen(true);
-	};
-
 	return (
 		<header className={styles.header}>
 			<div className={styles.container}>
@@ -32,19 +20,9 @@ const Header = (props: Props): JSX.Element => {
 					<img src={logo} alt='PartyPlanner.io' />
 				</div>
 				<div className={styles.headerButtons}>
-					<button className={styles.button} onClick={openSearchModal}>Search</button>
-					<button className={styles.logoutButton}>log out</button>
+					{props.children}
 				</div>
-				<div className={styles.headerButtons}>{props.children}</div>
 			</div>
-			<ReactModal
-				overlayClassName={styles.overlay}
-				className={styles.modal}
-				isOpen={searchModalOpen}
-				ariaHideApp={false}
-			>
-				<SearchParty closeModal={closeSearchModal} />
-			</ReactModal>
 		</header>
 
 	);
