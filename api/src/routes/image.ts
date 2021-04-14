8e7635e2f854fcad04a143e3c19967fd954ee7af
Sayed Sayed <sayed.sayed@student.manchester.ac.uk>
2021-04-14 07:26:08 +0100

    Completed image model but few tests failing

--- api/src/routes/image.ts
+++ api/src/routes/image.ts
@@ -9,22 +9,45 @@ export default imageRouter;
 
 //Upload Image 
 imageRouter.post(
-    '/upload/:uid/:pid/:link',
+    '/uploadimage/:uid/:pid/:link',
 
     async (req: express.Request, res: express.Response) => {
 
         try {
-            const newImage: IImage = new Image({
+            const ImageToAdd: IImage = new Image({
                 userId: req.params.uid,
                 partyId: req.params.pid,
                 link: req.params.link,
             });
+
+            const newImage: IImage = new Image(ImageToAdd);
+			await newImage.save();
+			res.status(200).json({ id: newImage._id, ...ImageToAdd });
         } catch (e) {
             res.status(500).json('Oops something went wrong');
         }
     }
 );
 
+// Fetch Party Images
+imageRouter.get(
+	'/party-image/:id',
+	async (req: express.Request, res: express.Response) => {
+		try {
+			const idTofind = req.params.id;
+			const foundImages = await Image.find({
+				partyId:idTofind,
+			});
+			if (_.isEmpty(foundImages)) {
+				res.status(404).send('You have no parties');
+			} else {
+				res.status(200).json(foundImages); // send to  party image html page
+			}
+		} catch (e) {
+			res.status(500).json('Oops something went wrong');
+		}
+	}
+);
 
 //Delete Image
 imageRouter.delete(
---
+++ api/tests/models/image.test.ts
@@ -0,0 +1,42 @@
+import { Image, IImage } from '../../src/models/image';
+import { ObjectId } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import * as mongoose from 'mongoose';
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+	mongoServer = new MongoMemoryServer();
+	const mongoUri = await mongoServer.getUri();
+	await mongoose
+		.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+		.then()
+		.catch(() => console.error('Unable to connect to MongoDB'));
+});
+
+afterAll(async () => {
+	await mongoose.disconnect();
+	await mongoServer.stop();
+});
+
+afterEach(async () => {
+	await Image.deleteMany({});
+});
+
+describe('Image model tests', () => {
+	it('Should correctly add a image to the db', async () => {
+		const newImage = {
+			_id: new ObjectId(),
+            userId: 'testuserid',
+            partyId: 'testpartyid',
+            link: 'testpartylink',
+			
+		};
+		const image: IImage = new Image(newImage);
+		const savedImage = await image.save();
+		expect(image).toEqual(savedImage);
+		const foundImage = await Image.findById(newImage._id);
+		expect(foundImage).toBeTruthy();
+	});
+
+});
