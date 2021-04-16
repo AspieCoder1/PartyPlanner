806df81eb178a43e743616b3a55c5ba10e629653
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-04-16 16:31:37 +0100

    Finished image grid

--- frontend/src/components/ImageSlider.tsx
+++ frontend/src/components/ImageSlider.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 import '../components/ImageSlider.css';
 import axios from 'axios';
 import { useParams } from 'react-router';
+import styles from './Image.module.scss';
+import * as _ from 'lodash';
 
 const apiRoute = process.env.REACT_APP_BACKEND_URL || '';
 
@@ -13,10 +14,7 @@ type Params = {
 
 const ImageSlider = (): JSX.Element => {
 	const { id } = useParams<Params>();
-	const [current, setCurrent] = useState(0);
 	const [slides, setSlides] = useState<string[]>([]);
-
-	//get images data by searching for partyID
 
 	const fetchData = async () => {
 		try {
@@ -31,32 +29,16 @@ const ImageSlider = (): JSX.Element => {
 		fetchData();
 	}, []);
 
-	const length = slides.length;
-
-	const nextSlide = () => {
-		setCurrent(current === length - 1 ? 0 : current + 1);
-	};
-
-	const prevSlide = () => {
-		setCurrent(current === 0 ? length - 1 : current - 1);
-	};
-
-	if (!Array.isArray(slides) || slides.length <= 0) {
-		return <></>;
-	}
-
 	return (
-		<section className='slider'>
-			<FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
-			<FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
-			{slides.map((slide, index) => {
+		<div className={styles.gallery}>
+			{ slides.length > 0 ? slides.map((link: string) => {
 				return (
-					<div className={index === current ? 'slide active' : 'slide'} key={index}>
-						{index === current && <img src={slide} alt='party image' className='image' />}
+					<div key={_.uniqueId()} className={styles.gridItem}>
+						<img  className={styles.image} src={link} />
 					</div>
 				);
-			})}
-		</section>
+			}) : null}
+		</div>
 	);
 };
 
--- frontend/src/components/ImageUpload.tsx
+++ frontend/src/components/ImageUpload.tsx
@@ -2,6 +2,8 @@ import React, { ChangeEvent, useState } from 'react';
 import ImageSlider from './ImageSlider';
 import { useParams } from 'react-router';
 import axios from 'axios';
+import Header from './shared/Header';
+import styles from './Image.module.scss';
 
 // Using this component:   <ImageUpload partyID={'testParty'} />  partyID needs to be passed
 
@@ -30,16 +32,16 @@ const ImageUpload = (): JSX.Element => {
 			formData.append('image', file);
 			await axios.post(`${apiRoute}/api/images/uploadimage/${id}`, formData, {
 				headers: {
-					'Content-Type': 'multipart/form-data'
-				}
+					'Content-Type': 'multipart/form-data',
+				},
 			});
 			alert('Image submitted');
 		}
-
 	};
 
 	return (
 		<>
+			<Header></Header>
 			<div>
 				<label>Select File </label>
 				<input type='file' name='file' accept='image/png, image/jpeg' onChange={onChange} />
@@ -56,57 +58,4 @@ const ImageUpload = (): JSX.Element => {
 	);
 };
 
-// class ImageUpload extends Component {
-// 	constructor(props) {
-// 		super(props);
-//
-// 		this.state = {
-// 			file: null,
-// 		};
-// 	}
-//
-// 	handleChange(e) {
-// 		let file = e.target.files[0];
-// 		this.setState({
-// 			file: file,
-// 		});
-// 	}
-//
-// 	handleUpload(e) {
-// 		const partyID = this.props.partyID;
-// 		console.log(partyID);
-//
-// 		let file = this.state.file;
-// 		let formData = new FormData();
-//
-// 		formData.append('image', file);
-//
-// 		const res = axios.post(`${apiRoute}/api/images/uploadimage/${partyID}`, { formData });
-// 		alert('Your image has been uploaded successfully');
-// 	}
-//
-// 	render() {
-// 		return (
-// 			<>
-// 				<div>
-// 					<label>Select File </label>
-// 					<input type='file' name='file' accept='image/png, image/jpeg' onChange={(e) => this.handleChange(e)} />
-// 					<button type='button' onClick={(e) => this.handleUpload(e)}>
-// 						Upload
-// 					</button>
-// 				</div>
-// 				<br />
-// 				<br />
-// 				<div>
-// 					<ImageSlider partyID={this.props.partyID} />
-// 				</div>
-// 			</>
-// 		);
-// 	}
-// }
-//
-// ImageUpload.propTypes = {
-// 	userName: PropTypes.string,
-// 	partyID: PropTypes.string,
-// };
 export default ImageUpload;
